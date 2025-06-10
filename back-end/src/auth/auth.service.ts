0e19d73fd38f4f2c59cc382d3cf9a93a9666aa5c
truongbvnedu <106640293+Hongtruongbvn@users.noreply.github.com>
2025-06-10 10:12:33 +0700

    Update auth.service.ts

--- back-end/src/auth/auth.service.ts
+++ back-end/src/auth/auth.service.ts
@@ -22,7 +22,7 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user._id };
+    const payload = { username: user.username, role: user.role, exp: user.xp };
     return {
       access_token: this.jwtService.sign(payload),
     };

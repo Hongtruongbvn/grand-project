5104df20486e628deb256bb42a2172a1d4ddd338
minhquan0101 <quan123587@gmail.com>
2025-06-22 12:15:21 +0700

    post-22/6

--- back-end/src/message/message.module.ts
+++ back-end/src/message/message.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { MessageController } from './message.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Message, MessageSchema } from './dto/create-message.dto';
+import { Message, MessageSchema } from './schema/message.schema';
+
 
 @Module({
   imports: [

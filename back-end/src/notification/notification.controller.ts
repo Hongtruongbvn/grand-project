697aed1f80e895e3552fd3b5ec57ade6691ae427
truongbvnedu <106640293+Hongtruongbvn@users.noreply.github.com>
2025-06-23 20:13:31 +0700

    up date request join

--- back-end/src/notification/notification.controller.ts
+++ back-end/src/notification/notification.controller.ts
@@ -1,17 +1,16 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Param, Delete, NotFoundException } from '@nestjs/common';
 import { NotificationService } from './notification.service';
-import { CreateNotificationDto } from './dto/create-notification.dto';
-import { UpdateNotificationDto } from './dto/update-notification.dto';
 
 @Controller('notification')
 export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    const notification = await this.notificationService.detectNotification(id);
+    if (!notification) {
+      throw new NotFoundException('Notification not found');
+    }
+    await this.notificationService.detectNotification(id);
+    return { message: 'Notification deleted successfully' };
+  }
 }

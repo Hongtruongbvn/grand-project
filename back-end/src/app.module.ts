450c04c298d9d364501727180c417006acb3a932
minhquan0101 <quan123587@gmail.com>
2025-07-12 19:30:04 +0700

    Update app.module.ts

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,24 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-// import { RoleModule } from './role/role.module';
-import { PostsModule } from './posts/post.module';
+import { PostsModule } from './posts/post.module'; // ✅ Giữ từ quan1
 import { CommentModule } from './comment/comment.module';
 import { ReportModule } from './report/report.module';
 import { BlockModule } from './block/block.module';
-import { StoryModule } from './story/story.module';
 import { MessageModule } from './message/message.module';
 import { ChatroomModule } from './chatroom/chatroom.module';
 import { GlobalRoleModule } from './global-role/global-role.module';
 import { TypeModule } from './type/type.module';
 import { InterestModule } from './interest/interest.module';
-
 import { ConfigModule } from '@nestjs/config';
 import { GroupModule } from './group/group.module';
-import { SearchModule } from './search/search.module'; // Nam thêm
+import { SearchModule } from './search/search.module';
 import { GroupRoleModule } from './group-role/group-role.module';
 import { GroupMemberModule } from './group-member/group-member.module';
 import { NotificationModule } from './notification/notification.module';
@@ -26,36 +23,33 @@ import { ChatroomMemberModule } from './chatroom-member/chatroom-member.module';
 import { FriendRequestModule } from './friend-request/friend-request.module';
 import { UploadsModule } from './uploads/upload.module';
 import { GroupPostModule } from './group-post/group-post.module';
+// import { ChatModule } from './chat/chat.module'; 
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGOOSE as string),
-
-    UserModule,
+    forwardRef(() => UserModule),
     AuthModule,
-    // RoleModule,
     PostsModule,
     CommentModule,
     TypeModule,
     InterestModule,
     ReportModule,
     BlockModule,
-    StoryModule,
     MessageModule,
-    ChatroomModule,
+    forwardRef(() => ChatroomModule),
     GlobalRoleModule,
     GroupModule,
     GroupRoleModule,
     GroupMemberModule,
     NotificationModule,
-    ChatroomMemberModule,
-    SearchModule, // Nam thêm
+    forwardRef(() => ChatroomMemberModule),
+    SearchModule,
     FriendRequestModule,
     UploadsModule,
     GroupPostModule,
+    // ChatModule,
   ],
   controllers: [AppController],
   providers: [AppService],
